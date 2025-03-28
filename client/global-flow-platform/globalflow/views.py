c992b8530953e7b8f083ea69fd9ccd321f64fbe2
Vinicius Morgado <vinicius.morgado@bsource.com.br>
2025-03-28 02:21:22 -0300

    Add base and index html

--- client/global-flow-platform/globalflow/views.py
+++ client/global-flow-platform/globalflow/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World")
+    # return HttpResponse("Hello, World")
+    return render(request, "index.html")
--- client/global-flow-platform/manager/urls.py
+++ client/global-flow-platform/manager/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import globalflow
+from globalflow import urls as gf_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(globalflow.urls))
+    path('', include(gf_urls))
 ]
