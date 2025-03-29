9ee1be7f3b466168152ff046b553008f5736f3b1
Vinicius Morgado <vinicius.morgado@bsource.com.br>
2025-03-29 12:34:25 -0300

    Add sales table migration and to be use with matplotlib integration

--- client/global-flow-platform/globalflow/admin.py
+++ client/global-flow-platform/globalflow/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TodoItem
+from .models import TodoItem, Sale
 
 # Register your models here.
 admin.site.register(TodoItem)
+admin.site.register(Sale)
--- client/global-flow-platform/globalflow/models.py
+++ client/global-flow-platform/globalflow/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Sale(models.Model):
+    item = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self):
+        return str(self.item)
